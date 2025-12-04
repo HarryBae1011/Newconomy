39476f1c5a75f0fd0518ea6f4ecebf67c1b2fa11
HarryBae1011 <06harry@naver.com>
2025-12-05 01:54:23 +0900

    settings

---
+++ src/main/java/com/newconomy/NewconomyApplication.java
@@ -0,0 +1,13 @@
+package com.newconomy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewconomyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewconomyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newconomy/newconomy/NewconomyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newconomy.newconomy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewconomyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
