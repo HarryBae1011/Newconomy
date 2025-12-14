48650718b7ae51bd1732acca254e3ba4abd99d31
HarryBae1011 <06harry@naver.com>
2025-12-14 15:14:05 +0900

    HTML 태그 제거 Utils

---
+++ src/main/java/com/newconomy/global/common/HtmlUtils.java
@@ -0,0 +1,22 @@
+package com.newconomy.global.common;
+
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
+
+public class HtmlUtils {
+
+    // HTML 태그 완전 제거
+    public static String removeHtmlTags(String html) {
+        return Jsoup.parse(html).text();
+    }
+
+    // 특정 태그만 허용
+    public static String cleanHtml(String html) {
+        return Jsoup.clean(html, Safelist.basic());
+    }
+
+    // 엔티티 변환 (&quot; → ")
+    public static String unescapeHtml(String html) {
+        return Jsoup.parse(html).text();
+    }
+}
