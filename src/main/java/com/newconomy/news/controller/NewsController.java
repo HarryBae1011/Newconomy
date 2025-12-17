ac96ad5586fea608e24756ba94e164a35968ac1a
HarryBae1011 <06harry@naver.com>
2025-12-17 17:36:46 +0900

    api url 변경

--- src/main/java/com/newconomy/news/controller/NewsController.java
+++ src/main/java/com/newconomy/news/controller/NewsController.java
@@ -4,12 +4,14 @@ import com.newconomy.global.response.ApiResponse;
 import com.newconomy.news.crawling.SearchService;
 import com.newconomy.news.dto.NewsResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "뉴스 컨트롤러")
 @RestController
 @RequestMapping("/api/news")
 @RequiredArgsConstructor
@@ -17,7 +19,7 @@ public class NewsController {
 
     private final SearchService searchService;
 
-    @GetMapping("/news")
+    @GetMapping("/search")
     @Operation(summary = "경제 뉴스 검색", description = "경제 뉴스를 크롤링 해오는 API 입니다.")
     public ApiResponse<NewsResponseDTO.NewsItemsResponseDTO> searchNews(
             @RequestParam String query,
