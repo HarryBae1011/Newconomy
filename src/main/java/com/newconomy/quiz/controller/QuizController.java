5e87519bfe9b712e9b486481cdd3676206d064b6
sammool <sammool2003@gmail.com>
2026-01-02 18:19:01 +0900

    [Fix] 퀴즈 생성후 반환값 List<Long> -> quizListDTO

--- src/main/java/com/newconomy/quiz/controller/QuizController.java
+++ src/main/java/com/newconomy/quiz/controller/QuizController.java
@@ -27,9 +27,9 @@ public class QuizController {
 
     @Operation(summary = "해당 뉴스의 퀴즈 생성", description = "퀴즈 생성 API, fastapi서버의 upstage api를 호출합니다 / QuizId를 List로 반환")
     @PostMapping("/generate/{newsId}")
-    public ApiResponse<List<Long>> generateQuiz(@PathVariable("newsId") Long newsId) {
-        List<Long> quizIds = quizGenerateService.generateQuiz(newsId);
-        return ApiResponse.onSuccess(quizIds);
+    public ApiResponse<List<QuizResponseDTO.QuizGenerateResponseDTO>> generateQuiz(@PathVariable("newsId") Long newsId) {
+        List<QuizResponseDTO.QuizGenerateResponseDTO> quizzes = quizGenerateService.generateQuiz(newsId);
+        return ApiResponse.onSuccess(quizzes);
     }
 
 //    @Operation(summary = "퀴즈 목록 조회")
--- src/main/java/com/newconomy/quiz/service/QuizGenerateService.java
+++ src/main/java/com/newconomy/quiz/service/QuizGenerateService.java
@@ -16,6 +16,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Slf4j
 @Service
@@ -27,7 +28,7 @@ public class QuizGenerateService {
     private final NewsRepository newsRepository;
     private final WebClient webClient;
 
-    public List<Long> generateQuiz(Long newsId) {
+    public List<QuizResponseDTO.QuizGenerateResponseDTO> generateQuiz(Long newsId) {
         News news = newsRepository.findById(newsId).orElseThrow(() ->
                 new EntityNotFoundException("뉴스를 찾을 수 없습니다"));
 
@@ -38,13 +39,15 @@ public class QuizGenerateService {
                 .bodyToMono(QuizResponseDTO.QuizListResponseDto.class)
                 .block();
 
+        if (responseDto == null || responseDto.getQuizList() == null) {
+            throw new IllegalStateException("퀴즈 생성 API 응답이 비어 있습니다");
+        }
+
         List<QuizResponseDTO.QuizGenerateResponseDTO> quizList = responseDto.getQuizList();
         List<Quiz> saved = quizRepository.saveAll(quizList.stream().map(QuizConverter::toQuizEntity)
                 .toList());
+        List<QuizResponseDTO.QuizGenerateResponseDTO> result = saved.stream().map(QuizConverter::toQuizDTO).toList();
         log.info("뉴스 ID {}로부터 {}개의 퀴즈가 성공적으로 생성 및 저장되었습니다.", newsId, saved.size());
-        return saved.stream()
-                .map(Quiz::getId)
-                .collect(Collectors.toList());
-
+        return result;
     }
 }
