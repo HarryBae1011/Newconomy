0f1593d30898a1d938e1e5b8de1dc45182fbf269
sammool <sammool2003@gmail.com>
2026-01-05 21:36:54 +0900

    [Refactor] @EntityGraph활용하여 N+1문제 해결

--- src/main/java/com/newconomy/quiz/repository/QuizAttemptRepository.java
+++ src/main/java/com/newconomy/quiz/repository/QuizAttemptRepository.java
@@ -2,8 +2,10 @@ package com.newconomy.quiz.repository;
 
 import com.newconomy.member.domain.Member;
 import com.newconomy.quiz.domain.QuizAttempt;
+import jakarta.persistence.Entity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +14,10 @@ import java.util.List;
 
 public interface QuizAttemptRepository extends JpaRepository <QuizAttempt, Long> {
 
+    @EntityGraph(attributePaths = {"quiz"})
     Page<QuizAttempt> findByMember(Member member, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"quiz"})
     @Query("select qa from QuizAttempt qa " +
             "where qa.member.id = :memberId " +
             "and qa.isCorrect = false ")
